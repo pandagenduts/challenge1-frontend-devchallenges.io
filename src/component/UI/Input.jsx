9769a866b3d705f8f64ca84c6b3cfe967e59f9e4
Donny Rendi <donny.rendi@yahoo.co.id>
2023-10-09 21:20:45 +0700

    little update on input & readme update

--- src/component/UI/Input.jsx
+++ src/component/UI/Input.jsx
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 import { clsx } from "clsx";
 
 const inputVariants = cva(
-  "px-3 outline outline-1 outline-[#828282] hover:outline-[#333] focus:outline-[#2962FF] duration-150 rounded-lg",
+  "px-3 outline outline-1 outline-[#828282] hover:outline-[#333] focus:outline-[#2962FF] duration-150 rounded-lg w-full",
   {
     variants: {
       size: {
@@ -19,6 +19,7 @@ const inputVariants = cva(
 );
 
 const Input = (props) => {
+  // turn true if input is on focus
   const [isInputFocused, setInputFocused] = useState(false);
 
   const handleInputFocus = () => {
@@ -44,16 +45,17 @@ const Input = (props) => {
     labelClassName,
   } = props;
 
+  // this is tailwind classes for input/textarea element
   const inputClasses = twMerge(
     inputVariants({ size, className }),
     error && "outline-[#D32F2F] focus:outline-[#D32F2F] hover:outline-[#333]",
-    disabled && "cursor-not-allowed outline-[#E0E0E0]",
-    fullWidth && "w-full",
-    startIcon && 'pl-11',
-    endIcon && 'pr-11',
-
+    disabled && "cursor-not-allowed outline-[#E0E0E0] hover:outline-[#E0E0E0]",
+    // fullWidth && "w-full",
+    startIcon && "pl-11",
+    endIcon && "pr-11"
   );
 
+  // tailwind classes for label
   const labelClasses = twMerge(
     clsx(
       "text-xs text-333 duration-150",
@@ -63,6 +65,7 @@ const Input = (props) => {
     )
   );
 
+  // tailwind classes for helper text
   const helperTextClasses = twMerge(
     clsx("text-828282 text-[10px] font-normal", error && "text-[#D32F2F]")
   );
@@ -76,8 +79,16 @@ const Input = (props) => {
       <label htmlFor="inputComponent" className={labelClasses}>
         Label
       </label>
-      <div className='relative w-auto input-wrapper'>
-        {startIcon && <span className="absolute text-xl left-3 material-icons top-4 text-828282">{startIcon}</span>}
+      <div
+        className={`relative ${
+          fullWidth ? "max-w-full" : "max-w-[200px]"
+        } input-wrapper`}
+      >
+        {startIcon && (
+          <span className={`absolute text-xl left-3 material-icons top-4 ${isInputFocused ? 'text-2962FF' : 'text-828282'} duration-150`}>
+            {startIcon}
+          </span>
+        )}
         {multiline ? (
           <textarea
             type="text"
@@ -104,7 +115,11 @@ const Input = (props) => {
             disabled={disabled}
           />
         )}
-        {endIcon && <span className="absolute text-xl material-icons top-4 right-3 text-828282">{endIcon}</span>}
+        {endIcon && (
+          <span className={`absolute text-xl material-icons top-4 right-3 ${isInputFocused ? 'text-2962FF' : 'text-828282'} duration-150`}>
+            {endIcon}
+          </span>
+        )}
       </div>
       {helperText && <span className={helperTextClasses}>{helperText}</span>}
     </div>
