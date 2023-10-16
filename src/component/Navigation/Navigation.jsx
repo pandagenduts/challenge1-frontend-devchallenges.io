efe1094cb251162a6300f71b09920f0037e7f801
Donny Rendi <donny.rendi@yahoo.co.id>
2023-10-16 16:53:11 +0700

    adding a hamburger toggle menu on mobile for better UI UX

--- src/component/Navigation/Navigation.jsx
+++ src/component/Navigation/Navigation.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import HamburgerIcon from "../../../public/hamburger.svg";
+import CloseIcon from "../../../public/close.svg";
 
 const Navigation = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <aside className="w-full p-4 md:pt-12 md:w-64 bg-FAFBFD">
-      <p className="mb-10 md:mb-[122px] text-[13px]">
-        <span className="text-[#F7542E] font-poppins font-semibold">Dev</span>challenges.io
-      </p>
-      <nav>
+      <div className="flex justify-between">
+        <p className="text-[13px]">
+          <span className="text-[#F7542E] font-poppins font-semibold">Dev</span>
+          challenges.io
+        </p>
+        {!showMenu && (
+          <img
+            src={HamburgerIcon}
+            alt="hamburger-icon"
+            className="cursor-pointer md:hidden"
+            onClick={() => setShowMenu(true)}
+          />
+        )}
+        {showMenu && (
+          <img
+            src={CloseIcon}
+            alt="close-icon"
+            className="cursor-pointer md:hidden"
+            onClick={() => setShowMenu(false)}
+          />
+        )}
+      </div>
+      <nav className={`${!showMenu && "hidden"} md:block mt-10 md:mt-[122px]`}>
         <ul className="flex flex-col gap-4 text-sm font-thin md:gap-6 font-montserrat">
           <li className="transition duration-150 hover:font-bold">
             <NavLink
